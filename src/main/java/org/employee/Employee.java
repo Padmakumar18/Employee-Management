b5b6cb11aa60605f9807e5acd31e20f3380c8a8d
padmakumar18 <padmakumar41759@gmail.com>
2025-02-28 17:18:40 +0530

    Need to solve issues

--- src/main/java/org/employee/Employee.java
+++ src/main/java/org/employee/Employee.java
@@ -1,15 +1,17 @@
 package org.employee;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
-    
+
     @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int emp_id;
-    
+
     private String emp_name;
     private String emp_department;
     private long emp_salary;
@@ -48,9 +50,12 @@ public class Employee {
 
     @Override
     public String toString() {
-        return "emp_id=" + emp_id +
-                ",\n emp_name='" + emp_name +
-                ", \n emp_department='" + emp_department +
-                ",\n emp_salary=" + emp_salary;
+        return String.format("+------------+----------------+-----------------+------------+\n"
+                + "| Employee ID | Name           | Department      | Salary     |\n"
+                + "+------------+----------------+-----------------+------------+\n"
+                + "| %-10d | %-14s | %-15s | %-10d |\n"
+                + "+------------+----------------+-----------------+------------+",
+                emp_id, emp_name, emp_department, emp_salary);
     }
+
 }
--- src/main/java/org/employee/Main.java
+++ src/main/java/org/employee/Main.java
@@ -7,145 +7,168 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         Configuration cfg = new Configuration();
-
         cfg.configure();
         cfg.addAnnotatedClass(org.employee.Employee.class);
-
         SessionFactory sf = cfg.buildSessionFactory();
-        Session session = sf.openSession();
-
-        Transaction transaction = session.beginTransaction();
 
         int option = 0;
-        Employee employee = new Employee();
         while (option < 7) {
             System.out.println("------------------------------------------");
             System.out.println("1. View all employee details");
             System.out.println("2. View particular employee detail");
             System.out.println("3. Add new employee");
-            System.out.println("4. Update a employee detail");
-            System.out.println("5. Delete a employee");
+            System.out.println("4. Update an employee detail");
+            System.out.println("5. Delete an employee");
             System.out.println("6. Delete all employee details");
             System.out.println("7. Exit");
             System.out.println("------------------------------------------");
 
-            System.out.println("Enter the choice");
+            System.out.print("Enter your choice: ");
             option = sc.nextInt();
 
             switch (option) {
                 case 1:
-                    displayAll(session);
+                    displayAll(sf);
                     break;
-                case 2: {
-                    System.out.println("Enter the employeeId : ");
-                    int emp_id = sc.nextInt();
-                    viewEmployee(session, employee, emp_id);
+                case 2:
+                    System.out.print("Enter the employee ID: ");
+                    int empId = sc.nextInt();
+                    viewEmployee(sf, empId);
                     break;
-                }
                 case 3:
-                    System.out.println("1. Enter employee name : ");
-                    employee.setEmp_name(sc.next());
-                    System.out.println("2. Enter employee department : ");
-                    employee.setEmp_department(sc.next());
-                    System.out.println("4. Enter salary : ");
-                    employee.setEmp_salary(sc.nextInt());
+                    sc.nextLine();
+                    Employee newEmployee = new Employee();
+                    System.out.print("Enter employee name: ");
+                    newEmployee.setEmp_name(sc.nextLine());
+                    System.out.print("Enter employee department: ");
+                    newEmployee.setEmp_department(sc.nextLine());
+                    System.out.print("Enter salary: ");
+                    newEmployee.setEmp_salary(sc.nextInt());
 
-                    addNewEmployee(session, employee, transaction);
-
-                    System.err.println(employee.toString());
+                    addNewEmployee(sf, newEmployee);
                     break;
-                case 4: {
-                    System.out.println("Enter the employeeID to update details ");
-                    int emp_id = sc.nextInt();
-                    System.out.println("Enter 1 for Name , 2 for Department , 3 for salary ");
+                case 4:
+                    System.out.print("Enter employee ID to update details: ");
+                    int updateId = sc.nextInt();
+                    System.out.println("Enter 1 for Name, 2 for Department, 3 for Salary: ");
                     int updateOption = sc.nextInt();
-                    switch (updateOption) {
-                        case 1:
-                            employee.setEmp_name(sc.nextLine());
-                            break;
-                        case 2:
-                            employee.setEmp_department(sc.nextLine());
-                            break;
-                        case 3:
-                            employee.setEmp_salary(sc.nextInt());
-                            break;
-                    }
-                    update(session, employee, emp_id);
+                    sc.nextLine();
+                    update(sf, updateId, updateOption, sc);
                     break;
-                }
                 case 5:
-                    System.out.println("Enter employee ID to delete");
-                    int emp_id = sc.nextInt();
-                    delete(session, transaction, emp_id);
+                    System.out.print("Enter employee ID to delete: ");
+                    int deleteId = sc.nextInt();
+                    delete(sf, deleteId);
                     break;
                 case 6:
-                    deleteAll(session, transaction);
+                    deleteAll(sf);
+                    break;
+                case 7:
+                    System.out.println("Exiting...");
                     break;
                 default:
-                    break;
+                    System.out.println("Invalid choice. Try again.");
             }
         }
 
-        transaction.commit();
-        session.close();
         sf.close();
+        sc.close();
     }
 
-    public static void update(Session session, Employee employee, int Id) {
+    public static void displayAll(SessionFactory sf) {
+        try (Session session = sf.openSession()) {
+            List<Employee> employees = session.createQuery("FROM Employee", Employee.class).list();
 
-    }
-
-    public static void viewEmployee(Session session, Employee employee, int emp_id) {
-        Employee emp = session.get(Employee.class, emp_id);
-        if (emp == null) {
-            System.out.println("DB Empty");
-        } else {
-            System.out.println(emp.toString());
+            if (employees.isEmpty()) {
+                System.out.println("No records found.");
+            } else {
+                employees.forEach(System.out::println);
+            }
         }
     }
 
-    public static void displayAll(Session session) {
-        Query query = session.createQuery("FROM Employee");
-        List<org.employee.Employee> employees = query.list();
-
-        if (employees == null) {
-            System.out.println("DB Empty");
-        } else {
-            for (org.employee.Employee emp : employees) {
+    public static void viewEmployee(SessionFactory sf, int empId) {
+        try (Session session = sf.openSession()) {
+            Employee emp = session.get(Employee.class, empId);
+            if (emp == null) {
+                System.out.println("Employee not found.");
+            } else {
                 System.out.println(emp);
             }
-            // System.out.println(employees.toString());
         }
-
     }
 
-    public static void delete(Session session, Transaction transaction, int emp_id) {
-
-        Employee emp = session.get(Employee.class, emp_id);
-        if (emp != null) {
-            session.remove(emp);
+    public static void addNewEmployee(SessionFactory sf, Employee employee) {
+        try (Session session = sf.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(employee);
+            transaction.commit();
+            System.out.println("Employee added successfully.");
         }
-        transaction.commit();
-        transaction = session.beginTransaction();
     }
 
-    public static void deleteAll(Session session, Transaction transaction) {
-        session.createQuery("DELETE FROM Employee").executeUpdate();
-        transaction.commit();
-        transaction = session.beginTransaction();
+    public static void update(SessionFactory sf, int empId, int option, Scanner sc) {
+        try (Session session = sf.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Employee emp = session.get(Employee.class, empId);
+
+            if (emp == null) {
+                System.out.println("Employee not found.");
+                return;
+            }
+
+            switch (option) {
+                case 1:
+                    System.out.print("Enter new name: ");
+                    emp.setEmp_name(sc.nextLine());
+                    break;
+                case 2:
+                    System.out.print("Enter new department: ");
+                    emp.setEmp_department(sc.nextLine());
+                    break;
+                case 3:
+                    System.out.print("Enter new salary: ");
+                    emp.setEmp_salary(sc.nextInt());
+                    break;
+                default:
+                    System.out.println("Invalid update option.");
+                    return;
+            }
+
+            session.merge(emp);
+            transaction.commit();
+            System.out.println("Employee updated successfully.");
+        }
     }
 
-    public static void addNewEmployee(Session session, Employee employee, Transaction transaction) {
-        session.save(employee);
-        transaction.commit();
-        transaction = session.beginTransaction();
+    public static void delete(SessionFactory sf, int empId) {
+        try (Session session = sf.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Employee emp = session.get(Employee.class, empId);
+
+            if (emp != null) {
+                session.remove(emp);
+                System.out.println("Employee deleted successfully.");
+            } else {
+                System.out.println("Employee not found.");
+            }
+
+            transaction.commit();
+        }
+    }
+
+    public static void deleteAll(SessionFactory sf) {
+        try (Session session = sf.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            int deletedCount = session.createQuery("DELETE FROM Employee").executeUpdate();
+            transaction.commit();
+            System.out.println(deletedCount + " records deleted.");
+        }
     }
 }
